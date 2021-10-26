4814b848a0d5a0a7b37374a80d7e9a1685184977
rakiadaly73 <57303000+rakiadaly73@users.noreply.github.com>
2021-10-26 19:29:56 +0100

    test unitaire + sonar

---
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -0,0 +1,107 @@
+package tn.esprit.spring.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.repository.DepartementRepository;
+import tn.esprit.spring.repository.EntrepriseRepository;
+
+@Service
+public class EntrepriseServiceImpl implements IEntrepriseService {
+
+	@Autowired
+    EntrepriseRepository entrepriseRepoistory;
+	@Autowired
+	DepartementRepository deptRepoistory;
+	
+	public int ajouterEntreprise(Entreprise entreprise) {
+		entrepriseRepoistory.save(entreprise);
+		return entreprise.getId();
+	}
+
+	public int ajouterDepartement(Departement dep) {
+		deptRepoistory.save(dep);
+		return dep.getId();
+	}
+	
+	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
+		//Le bout Master de cette relation N:1 est departement  
+				//donc il faut rajouter l'entreprise a departement 
+				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
+				//Rappel : la classe qui contient mappedBy represente le bout Slave
+				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
+
+		Entreprise entreprise = new Entreprise();
+		Optional<Entreprise> t = entrepriseRepoistory.findById(entrepriseId);
+		if(t.isPresent()) {
+		entreprise = t.get();}
+		entrepriseRepoistory.save(entreprise);
+
+     	Departement departement = new Departement();
+	    Optional<Departement> d = deptRepoistory.findById(depId);
+	    if(d.isPresent()) {
+		departement = d.get();}
+	    deptRepoistory.save(departement);
+
+				
+				departement.setEntreprise(entreprise);
+				deptRepoistory.save(departement);
+		
+	}
+	
+	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
+		Entreprise entreprise = new Entreprise();
+		Optional<Entreprise> t = entrepriseRepoistory.findById(entrepriseId);
+		if(t.isPresent()) {
+		entreprise = t.get();}
+		entrepriseRepoistory.save(entreprise);
+
+		List<String> depNames = new ArrayList<>();
+		for(Departement dep : entreprise.getDepartements()){
+			depNames.add(dep.getName());
+		}
+		
+		return depNames;
+	}
+
+	@Transactional
+	public void deleteEntrepriseById(int entrepriseId) {
+		Entreprise entreprise = new Entreprise();
+		Optional<Entreprise> t = entrepriseRepoistory.findById(entrepriseId);
+		if(t.isPresent()) {
+		entreprise = t.get();}
+		entrepriseRepoistory.save(entreprise);
+
+	}
+
+	@Transactional
+	public void deleteDepartementById(int depId) {
+		Departement departement = new Departement();
+	    Optional<Departement> d = deptRepoistory.findById(depId);
+	    if(d.isPresent()) {
+		departement = d.get();}
+	    deptRepoistory.save(departement);	
+	}
+
+
+	public Entreprise getEntrepriseById(int entrepriseId) {
+		 Entreprise entreprise = new Entreprise();
+		Optional<Entreprise> t = entrepriseRepoistory.findById(entrepriseId);
+		if(t.isPresent()) {
+		entreprise = t.get();}
+		entrepriseRepoistory.save(entreprise);
+		return entreprise ;
+
+	}
+	
+	public List<Entreprise> getAllEntreprises(){
+		return (List<Entreprise>) entrepriseRepoistory.findAll();
+	}
+}
---
+++ src/test/java/tn/esprit/spring/tests/EntrepriseServiceImplTest.java
@@ -0,0 +1,89 @@
+package tn.esprit.spring.tests;
+
+
+import static org.junit.Assert.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.services.EntrepriseServiceImpl;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntrepriseServiceImplTest {
+
+	private static final Logger l = (Logger) LogManager.getLogger(EntrepriseServiceImplTest.class);
+	
+	@Autowired
+	EntrepriseServiceImpl es;
+	
+	@Test
+	public void testAddEntreprise() {
+		try {
+		Entreprise E = new Entreprise("Samsung","EURL");
+		int Id = es.ajouterEntreprise(E);
+		assertNotNull(Id);
+		es.deleteEntrepriseById(Id);
+		l.info("Add Entreprise works");
+		} catch (NullPointerException e) {
+			l.error(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testUpdateEntreprise() {
+		try {
+		Entreprise E = new Entreprise("Samsung","EURL");
+		int Id = es.ajouterEntreprise(E);
+		E.setName("Iphone");
+		es.ajouterEntreprise(E);
+		Entreprise Ese = es.getEntrepriseById(Id);
+		assertEquals("Iphone",Ese.getName());
+		es.deleteEntrepriseById(Id);
+		l.info("Update Entreprise works");
+		} catch (NullPointerException e) {
+			l.error(e.getMessage());
+		}
+	}
+	
+	//test % size of list 
+	@Test
+	public void testDeleteEntrepriseById_METHOD1() {
+		try {
+		Entreprise E = new Entreprise("Samsung","EURL");
+		int Id = es.ajouterEntreprise(E);
+		int lengthBeforeDelete = es.getAllEntreprises().size();
+		es.deleteEntrepriseById(Id);
+		assertEquals(lengthBeforeDelete-1 , es.getAllEntreprises().size());
+		l.info("Delete Entreprise (%size) works");
+		} catch (NullPointerException e) {
+			l.error(e.getMessage());
+		}
+	}
+	@Test
+	public void testAffectDepartmentToEntreprise(){
+		try {
+		Entreprise E = new Entreprise("Samsung","EURL");
+		int IdE = es.ajouterEntreprise(E);
+		Departement D = new Departement("Info");
+		int IdD = es.ajouterDepartement(D);
+		assertNull(D.getEntreprise());
+		es.affecterDepartementAEntreprise(IdD, IdE);
+		assertNotNull(D.getEntreprise().getId());
+		assertEquals(D.getEntreprise().getId(),IdE);
+		es.deleteDepartementById(IdD);
+		es.deleteEntrepriseById(IdE);
+		l.info("Affect Department to Entreprise works");
+		} catch (NullPointerException e) {
+			l.error(e.getMessage());
+		}
+	}
+}
